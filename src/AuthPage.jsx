1ba7ee8c81bda2895742d233c5568cdc194fda79
Kert Sai <kertsayson@gmail.com>
2024-07-17 12:05:10 +0800

    changed css

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -14,9 +14,12 @@ const AuthPage = (props) => {
   return (
     <div className="background">
       <form onSubmit={onSubmit} className="form-card">
-        <div className="form-title">Welcome 👋</div>
+        <div className="form-title">Welcome!</div>
 
-        <div className="form-subtitle">Set a username to get started</div>
+        <div className="form-subtitle">
+          Set a username to get started, if username was set previously please
+          type previous username to log in
+        </div>
 
         <div className="auth">
           <div className="auth-label">Username</div>
